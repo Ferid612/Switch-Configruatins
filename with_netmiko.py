75833b63b63ef4467fc245f117e2ee7b71d27598
Ferid612 <hebibliferid20@gmail.com>
2023-11-29 13:14:31 +0400

    Last updates

--- with_netmiko.py
+++ with_netmiko.py
@@ -3,7 +3,7 @@ import os
 import time
 from netmiko import ConnectHandler, NetmikoTimeoutException
 
-def connect_to_switch(ip_address, username="admin", password="!n\/esT@"):
+def connect_to_switch(ip_address, username = "admin", password = "!n\/esT@"):
     switch = {
         'device_type': 'hp_comware_telnet',
         'host': ip_address,
@@ -41,38 +41,54 @@ def send_command_to_switch(connection, command = None, sleep_time = 1 ):
     print(output)
     return output
         
+        
 
+def start_backup_procedure(ip_address, password = None):
+    connection = connect_to_switch(ip_address, password = password)
+
+    login_to_switch(connection) 
+
+    output = send_command_to_switch(connection, "disp curr")
+
+    all_text = output
+    while True:
+        output = send_command_to_switch(connection, command = "-", sleep_time=0)
+        all_text += " \n " + output
+        if "<" in output:
+            break
+
+    
+    
+    write_to_text(ip_address,all_text) 
+    print(output)  # Print the command output
+    connection.disconnect()  # Disconnect from the device
+
+
+    
+    
 def backup_switches(ip_addresses):
     error_ips = []
     count = 0
     for ip_address in ip_addresses:
         count += 1 
         print(f"============== {count}/ {len(ip_addresses)} ==============")
-        print(f"============ {ip_address} ==============")
-        try:
-            connection = connect_to_switch(ip_address)
-        
-            login_to_switch(connection) 
-        
-            output = send_command_to_switch(connection, "disp curr")
-        
-            all_text = output
-            while True:
-                output = send_command_to_switch(connection, command= "-", sleep_time=0)
-                all_text += " \n " + output
-                if "<" in output:
-                    break
-
-            
-            
-            write_to_text(ip_address,all_text) 
-            print(output)  # Print the command output
-            connection.disconnect()  # Disconnect from the device
-
-    
+        print(f"=========== {ip_address} ==============")
+        try:   
+            start_backup_procedure(ip_address)
         except Exception as e:
-            error_ips.append(ip_address)
-            print(f"Error: {str(e)}")
+            try:
+                try_again_count = 1
+                start_backup_procedure(ip_address, password="AssKlim{}+#$")
+            except:
+                try:
+                    try_again_count+=1
+                    start_backup_procedure(ip_address, password="Switch")
+                except:
+                    try_again_count+=1
+                    pass
+            if try_again_count == 3:
+                error_ips.append(ip_address)
+                print(f"Error: {str(e)}")
 
     if error_ips:
         file_name = f"error_reports/error_hp_switches_{current_time}.txt"
@@ -104,7 +120,8 @@ def write_to_text(ip_address, all_text):
 # ip_addresses = ["172.16.52.1"]
 # cisco_ip_addresses = ["172.16.73.1","172.16.70.1","172.16.70.222","172.16.70.14","172.16.77.1","172.16.77.12","172.16.47.1","172.16.78.1","172.16.92.1","172.16.91.1","172.16.97.1","172.16.93.1","172.16.93.210","172.16.99.1","172.16.99.100","172.16.80.1","172.16.32.1","172.16.55.1","172.16.90.1","172.16.90.70","172.16.90.150","172.16.90.217","172.16.90.67","172.16.90.36","172.16.90.69","172.16.31.1","172.16.60.1","172.16.60.135","172.16.60.30","172.16.60.160","172.16.72.1","172.16.75.1","172.16.73.1","172.16.46.1"]
 # hp_ip_addresses = ["172.16.81.1","172.16.82.1","172.16.44.1","172.16.42.1","172.16.41.1","172.16.43.1","172.16.40.1","172.16.70.116","172.16.70.180","172.16.70.240","172.16.30.1","172.16.30.76","172.16.30.100","172.16.90.223","172.16.96.1","172.16.95.1","172.16.54.1","172.16.94.1","172.16.53.1","172.16.53.202","172.16.71.1","172.16.50.1","172.16.52.1","172.16.51.1","172.16.74.1","10.3.22.6 "]
-error_hp_ip_addresses = ["172.16.41.1", "172.16.40.1", "172.16.70.180", "172.16.30.1", "172.16.90.223", "172.16.74.1", "10.3.22.6"]
+# switches_without_telnet = ["172.16.41.1", "172.16.40.1", "172.16.70.180", "172.16.30.1", "172.16.90.223"]
+error_hp_ip_addresses = [ "172.16.74.1", "10.3.22.6"]
 # error_hp_ip_addresses = ["172.16.40.1"]
 
 
